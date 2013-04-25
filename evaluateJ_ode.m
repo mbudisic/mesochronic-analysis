7534ef16b3b6433c044b8b994390cacb95bc21a5
Marko Budisic <mbudisic@gmail.com>
2013-04-25 13:10:22 -0700

    added varying initial time to evaluateJ_ode

--- evaluateJ_ode.m
+++ evaluateJ_ode.m
@@ -1,15 +1,18 @@
-function [mJ, sol] = evaluateJ_ode( order, ic, f, T, h, dp )
-% EVALUATEJ_ODE
+function [mJ, sol] = evaluateJ_ode( order, ic, f, t0, T, h, dp )
+% EVALUATEJ_ODE( order, ic, f, t0, T, h, dp )
 %
 % Evaluate mesochronic Jacobian using direct method.
 %
 % order - order of the method used
-% ic - vector of initial condition
-% f, Jf - flow field and its jacobian (function handles)
-% T - vector of integration lengths
+% ic - a single initial condition (column vector)
+% f - flow field (vectorized function handle)
+% t0 - initial time (scalar)
+% T - vector of integration lengths (positive)
 % h - resampling time
+% dp - finite difference used for evaluation of the instantaneous Jacobian
 
 validateattributes(ic, {'numeric'}, {'column'})
+validateattributes(t0, {'numeric'}, {'scalar'})
 
 assert( min(diff(T)) > h, 'return steps should differ by more than integration step')
 assert( h <= min(T), 'time step should be smaller than integration length' )
@@ -17,8 +20,8 @@ assert( dp < 1, 'dp should be small')
 
 Tmax = max(T);
 
-t = 0:h:Tmax;
-tc = num2cell(t);
+t = t0 + ( 0:h:Tmax );
+
 % simulate
 opts = odeset('vectorized','on','RelTol',1e-4,'AbsTol',1e-6);
 [tout, yout] = ode23t(f, t, ic, opts);
@@ -28,21 +31,13 @@ sol.x = yout;
 
 assert( norm( t - tout.', Inf) < 1e-12, 'Output times do not match input times');
 
-%fprintf(1, 'Min dt:%e, Max dt:%e \n', min(diff(S.x)), max(diff(S.x)));
 
-% convert to cell
-y = num2cell(yout.',1);
-% 
-% 
-% % jacobians
-% Ji = cellfun(Jf, tc, y , 'UniformOutput', false);
-% Ji = cat(3, Ji{:});
-
-% evaluate using finite differences
+% evaluate Jacobian using finite differences
 Ji = jacobian_fd(f, tout, yout, dp);
 
 % return steps are multiples of resampling interval
 retstep = fix(T/h);
-%    [mJ, ti] = mcjacobian_mex(dt, cat(3,Ji{:}), 1000, 2);
+
+% solve the Jacobian equation
 mJ = mcjacobian_mex(h, Ji, retstep, order);
 
