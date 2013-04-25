f6b037710d9c20b788353ca97a4d29fd9f77f717
Marko Budisic <mbudisic@gmail.com>
2013-04-25 13:10:48 -0700

    Variable name change in jacobian_fd

--- jacobian_fd.m
+++ jacobian_fd.m
@@ -1,15 +1,16 @@
-function Ji = jacobian_fd(f, t, y, delta)
-% JACOBIAN_FD(f, t, y, delta)
+function Ji = jacobian_fd(f, t, y, dp)
+% JACOBIAN_FD(f, t, y, dp)
 %
-% Evaluate the instantaneous jacobian by central finite difference.
+% Evaluate the instantaneous jacobian by central finite difference
+% along trajectory (t_i, y_i)
 %
 % f - @(t,x) vector field
 % t - Npoints x 1 column vector of times
 % y - Npoints x Ndim matrix of states
-% delta - finite variation
+% dp - finite variation
 %
 % optimal delta is 
-% delta = ( 3 * eps * norm(f) / 2 norm( d^3 f )  )^(1/3)
+% dp = ( 3 * eps * norm(f) / 2 norm( d^3 f )  )^(1/3)
 % where norm(f) is infty-bound on values of f
 %       norm(d^3 f) is infty-bound on third derivative of f
 %       eps - machine precision (eps command in matlab)
@@ -20,10 +21,13 @@ Npoints = size(t,1);
 assert( size(y,1) == Npoints, 'Number of states has to match length of time vector');
 Ndim = size(y,2);
 
+% output matrix
 Ji = zeros(Ndim,Ndim,Npoints);
 
-p_var = kron( eye(Ndim), [1,-1]) * delta; % Ndim x 2Ndim
+% variations
+p_var = kron( eye(Ndim), [1,-1]) * dp; % Ndim x 2Ndim
 
+% evaluate Jacobian point by point - this can probably be vectorized
 for k = 1:Npoints
     
     t_p = t(k);
@@ -35,7 +39,7 @@ for k = 1:Npoints
         f_var(:,n) = f( t_p, point + p_var(:,n) );
     end
     
-    Ji(:, :, k) = ( f_var(:,1:2:end) - f_var(:,2:2:end) )/(2*delta);
+    Ji(:, :, k) = ( f_var(:,1:2:end) - f_var(:,2:2:end) )/(2*dp);
 end
 
 
