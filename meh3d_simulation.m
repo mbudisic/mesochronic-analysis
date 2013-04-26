8d2692c54106852cf4fa4247810c70c7f41b7093
Marko Budisic <mbudisic@gmail.com>
2013-04-26 11:28:25 -0700

    meh3d_simulation works

--- meh3d_simulation.m
+++ meh3d_simulation.m
@@ -1,10 +1,11 @@
-function retval = meh3d_simulation(f, T, method, ics, h, dp, order, name)
-% meh3d_simulation(f, T, method, N, h, dp, order, name)
+function retval = meh3d_simulation(f, t0, T, method, ics, h, dp, order, name)
+% meh3d_simulation(f, t0, T, method, N, h, dp, order, name)
 %
 % Compute mesohyperbolicity for the vector field f. (2d on [-0.5,0.5]^2
 % grid for now)
 %
 % f - vector field
+% t0 - initial time
 % T - vector of periods of averaging
 % mytype - 'ode' or 'fd' method of computation
 % ics - Npoints x 3 list of initial conditions
@@ -40,12 +41,52 @@ validateattributes(ics, {'numeric'}, {'ncols',3})
 Npoints = size(ics,1);
 Ndim = size(ics,2);
 
-filename = sprintf('system%s_%s_T_%.1f_N_%03d',name,method, max(T), N);
+filename = sprintf('system%s_%s_T_%.1f_N_%05d.mat',name,method, max(T), Npoints);
 
-Jacobians = cell(Npoints,1);
+fileexists = exist(filename, 'file');
 
+if fileexists
+    disp([filename ' exists. Loading from it.']);
+    retval = load(filename);
+else
+    retval = struct;
+end
 
+t1 = tic;
+if isfield(retval, 'Jacobians') && isfield(retval,'ics')
+    if (size(retval.Jacobians{1}, 1) == Ndim) && all( ics(:) == retval.ics(:) )
+        disp([filename ' has Jacobian data. Skipping computation']);
+        Jacobians = retval.Jacobians;
 
+    else
+        error('Jacobian data does not match input data. Exiting');
+    end
+    
+else
+    
+    
+    Jacobians = cell(Npoints,1);
+    
+    % first just compute Jacobians
+    disp([filename ': Started Jacobian computation.']); pause(0.5);
+    
+    parfor k = 1:Npoints
+        ic = ics(k, :).';
+        
+        if strcmpi(method,'ode')
+            mJ = evaluateJ_ode( order, ic, f, t0, T, h, dp );
+        else
+            mJ = evaluateJ_fd( ic, f, T, h, dp );
+        end
+        
+        Jacobians{k} = mJ;
+        
+    end
+    
+    fprintf(1, '%s : Jacobians computed in %.2f sec. Started classification.', filename, toc(t1));
+    pause(0.5);
+    
+end
 
 Dets = zeros([Npoints, length(T)]);
 Traces = zeros(size(Dets));
@@ -53,27 +94,7 @@ Meh = zeros(size(Dets));
 Compr = zeros(size(Dets));
 Nml = zeros(size(Dets));
 Defect = zeros( size(Dets));
-
-if Ndim == 3
-    TrCof = zeros(size(Dets));
-end
-
-% first just compute Jacobians
-disp([filename ': Started Jacobian computation.']);
-parfor k = 1:Npoints
-    ic = ics(k, :).';
-    
-    if strcmpi(method,'ode')
-        mJ = evaluateJ_ode( order, ic, f, T, h, dp );
-    else
-        mJ = evaluateJ_fd( ic, f, T, h, dp );
-    end
-    
-    Jacobians{k} = mJ;
-
-end
-
-disp([filename ': Jacobians computed. Started classification.']);
+TrCof = zeros(size(Dets));
 
 % evaluate quantifiers for Jacobians
 parfor k = 1:Npoints
@@ -85,14 +106,12 @@ parfor k = 1:Npoints
     myNml = zeros(size(myDets));
     myDefect = zeros(size(myDets));
     
-    if Ndim == 3
-        myTrCof = zeros(size(myDets));
-    end
-
+    myTrCof = zeros(size(myDets));
+    
     
     for n = 1:length(T)
         
-        [classes, compr, spectral] = meh3d( T(n), {Jacobians{k}(:,:,n)} );
+        [classes, quants, spectral] = meh3d( T(n), {Jacobians{k}(:,:,n)}, 1e-3 );
         
         % spectral
         myDets(n) = spectral.Dets;
@@ -100,11 +119,11 @@ parfor k = 1:Npoints
         if Ndim == 3
             myTrCof(n) = spectral.TrCofs;
         end
-
+        
         % quants
-        myCompr(n) = quants.compr;
-        myNml(n) = quants.nml;
-        myDefect(n) = quants.defect;
+        myCompr(n) = quants.Compr;
+        myNml(n) = quants.Nml;
+        myDefect(n) = quants.Defect;
         
         % classes
         myMeh(n) = classes;
@@ -117,14 +136,12 @@ parfor k = 1:Npoints
     Compr(k,:) = myCompr(:);
     Nml(k,:) = myNml(:);
     Defect(k,:) = myDefect(:);
-    
-    if Ndim == 3
-        TrCof(k,:) = myTrCof(:);
-    end
+    TrCof(k,:) = myTrCof(:);
     
     
 end
-disp([filename ': All done']);
+fprintf(1, '%s : done in %.2f sec.', filename, toc(t1));
+pause(0.5);
 
 retval.Jacobians = Jacobians;
 
@@ -147,5 +164,5 @@ if Ndim == 3
     retval.TrCof = TrCof;
 end
 
-save([filename '.mat'], '-struct','retval')
+save(filename, '-struct','retval')
 
