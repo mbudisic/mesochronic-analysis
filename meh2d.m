0e5fbe5c180c71fe466d61921435071babd35b53
Marko Budisic <mbudisic@gmail.com>
2013-05-21 10:21:56 -0600

    Meh2d now operates on a stack of Jacobian all integrated at different times

--- meh2d.m
+++ meh2d.m
@@ -1,22 +1,21 @@
-function [classes, quants, spectral] = meh2d( T, J )
+function [classes, quants, spectral] = meh2d( Ts, Js )
 % meh2d( T, tol , ... )
 %
 % inputs:
 %
-% T - integration period
-% J - cell array of 2x2 Jacobian matrices (if a single matrix is to be
-%     analyzed, pass it as {J})
+% T - vector of integration periods (length K)
+% J - mesochronic Jacobian, 2 x 2 x K
 %
 %
-% returns:
-% 
+% all the return fields are vectors of size of T
+%
 % -- classes
 %    Mesohyperbolicity class
 %    -1 - hyperbolicity, orientation preserving
 %    0  - ellipticity
 %    1  - hyperbolicity, orientation reversing
 %
-% -- quants.compr
+% -- quants.Compr
 % Compressibility of mesochronic Jacobian J (theoretically identical to 0):
 %   T * det J + tr J
 %   This is a coarse measure of numerical error.
@@ -24,81 +23,90 @@ function [classes, quants, spectral] = meh2d( T, J )
 % -- quants.NonNml
 % Non-normality of mesochronic Jacobian J: Frobenius norm of the commutator
 %   || J* . J - J . J*||
-%   When non-normality is zero, J has complete orthogonal basis of eigenvectors 
+%   When non-normality is zero, J has complete orthogonal basis of eigenvectors
 %   (it is *unitarily* diagonalizable)p.
 %
 % -- quants.Hyp
-% Hyperbolicity of mesochronic Jacobian J: 
+% Hyperbolicity of mesochronic Jacobian J:
 %   (T^2 * det J - 4) .* det J
 %   When positive, J has a pair of real eigenvalues (flow map is hyperbolic for the integration time).
 %
 % -- quants.NonDefect
-% Non-defectiveness of mesochronic Jacobian J: 
+% Non-defectiveness of mesochronic Jacobian J:
 %   smallest distance between roots of the minimal polynomial of J
-%   When zero, J is defective, i.e., it has an 
+%   When zero, J is defective, i.e., it has an
 %   incomplete basis of eigenvectors (it is not diagonalizable)
-% 
+%
 % -- quants.FTLE
-%    Finite Time Lyapunov Exponent 
+%    Finite Time Lyapunov Exponent
 %    (1/T) * log norm(J)
-%    norm is 2-norm, T is always positive 
+%    norm is 2-norm, T is always positive
 %
 % -- spectral.Dets - determinants of mc. Jacobians
-% -- spectral.Traces - traces of mc. Jacobians, 
+% -- spectral.Traces - traces of mc. Jacobians,
 
 
 
-validateattributes(T, {'numeric'},{'scalar','nonnegative','finite'} );
-validateattributes(J, {'cell'}, {});
-cellfun( @(x)validateattributes(x, {'numeric'}, {}), J)
+validateattributes(Ts, {'numeric'},{'nonnegative','finite'} );
+validateattributes(Js, {'numeric'}, {});
 
-% inputs are Jacobian matrices
-[Cp, Mp] = matpolys(J); % extract characteristic and minimal polynomials
+assert( numel(Ts) == size(Js,3), 'Number of Jacobians and integration periods has to be the same');
 
-% 3d polynomial
-% P = x^2 - x Trace + Det
+K = numel(Ts);
 
-if iscell(Cp)
-    Traces = cellfun( @(x)(-x(2)), Cp );
-    Dets = cellfun( @(x)(x(3)), Cp );
-else
-    Traces = -Cp(:,2);
-    Dets = Cp(:,3);
+% storage
+classes = nan(size(Ts));
+quants.Compr = zeros(size(Ts));
+quants.FTLE = zeros(size(Ts));
+quants.Hyp = zeros(size(Ts));
+quants.NonNml = zeros(size(Ts));
+quants.NonDefect = zeros(size(Ts));
+
+spectral.Traces = zeros(size(Ts));
+spectral.Dets = zeros(size(Ts));
+
+for k = 1:K
+    
+    % select appropriate averaging interval and Jacobian matrix
+    T = Ts(k);
+    J = Js(:,:,k);
+    
+    %% Compute quantifiers
+    
+    % Finite-Time Lyapunov Exponents
+    quants.FTLE(k) = ftle(T,J);
+    
+    % non-normality: Frobenius norm of the commutator of Jacobian
+    quants.NonNml(k) = norm( ctranspose(J)*J - J*ctranspose(J), 'fro' );
+    
+    % defect: smallest distance between roots of the minimal polynomial
+    quants.NonDefect(k) = mindist( roots( minpoly(J)  ) ); % mindist.m distributed with package
+    
+    % characteristic polynomial
+    Cp = poly(J);
+    Det = Cp(3);
+    Trace = -Cp(2);
+    spectral.Traces(k) = Trace;
+    spectral.Dets(k) = Det;
+    
+    % compressibility
+    quants.Compr(k) = T * Det + Trace;
+    
+    % mesohyperbolicity
+    quants.Hyp(k) = (T^2 * Det - 4) .* Det;
+    
+    %% computation of mesohyperbolicity
+    
+    if quants.Hyp(k) > 0 % mesohyperbolicity
+        
+        if Det >= 4/(T^2) % orientation reversing
+            classes(k) = 1;
+        elseif Det <= 0     % orientation preserving
+            classes(k) = -1;
+        else
+            error('Detected mesohyperbolicity, but cannot detect the class');
+        end
+    else
+        classes(k) = 0; % mesoellipticity
+    end
 end
-
-if ~iscell(Mp)
-    Mp = mat2cell(Mp, ones(1,size(Mp,1)), size(Mp,2));
-end
-
-%% Compute quantifiers
-
-% Finite-Time Lyapunov Exponents
-quants.FTLE = cellfun(  @(M)ftle(T,M), J );
-
-quants.Hyp = (T^2 * Dets - 4) .* Dets; % mesohyperbolic when positive
-
-% non-normality: Frobenius norm of the commutator of Jacobian
-quants.NonNml = cellfun( @(A)norm( ctranspose(A)*A - A*ctranspose(A), 'fro' ), J  );
-
-% defect: smallest distance between roots of the minimal polynomial
-quants.NonDefect = cellfun( @(p) mindist( roots(p) ), Mp ); % mindist.m distributed with package
-
-% compressibility
-quants.Compr = T * Dets + Traces;
-
-%% computation of mesohyperbolicity
-
-% figure out which indices of initial conditions correspond to each class
-classes_ind.hyp = quants.Hyp >= 0;
-
-classes_ind.mh_flipping = (Dets >= 4/(T^2)) & classes_ind.hyp;
-classes_ind.mh_pure = (Dets <= 0) & classes_ind.hyp;
-
-% assign pseudocolors
-classes = nan(size(Dets));
-classes(classes_ind.mh_flipping) = 1;
-classes(classes_ind.mh_pure) = -1;
-classes(~classes_ind.hyp) = 0;
-
-spectral.Dets = Dets;
-spectral.Traces = Traces;
