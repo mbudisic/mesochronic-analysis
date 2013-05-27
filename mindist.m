344726b8b364bc55995115ff5e9dddd195c5ea7f
Marko Budisic <mbudisic@gmail.com>
2013-05-26 18:06:39 -0700

    mindist reverted - new edits made all outputs equal to zero

--- mindist.m
+++ mindist.m
@@ -3,18 +3,20 @@ function d = mindist(v)
 %
 % Compute minimal distance between elements of v.
 
-Nel = numel(v);
-v = v(:);
+if ~iscolumn(v)
+    v = v.';
+end
 
-if max(v) == 0.
-%if all( v == 0 )
+if max(abs(v)) == 0.
     d = 0.;
 else
     % repeat vector to the matrix
-    V = repmat( v, [1, Nel]);
+    V = repmat( v, [1, numel(v)]);
     
-    % off-diagonal distances between elements (diagonal is obviously zero)
-    D = triu( abs(V - V.'), 1);
+    % pairwise distance between elements
+    D = abs(V - V.');
     
-    d = min( D(:) );
+    % minimal off-diagonal distance (diagonal is obviously zero)
+    d = min( D( eye(numel(v)) ~= 1 ) );
+    
 end
