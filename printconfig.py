6c368144de51b0ba808e8f57912bb116408f6da7
Marko Budisic <mbudisic@gmail.com>
2013-09-12 08:45:33 -0700

    princonfig now prints a bit extra

--- printconfig.py
+++ printconfig.py
@@ -2,7 +2,9 @@
 
 from scipy.io import loadmat
 import sys
-a = loadmat(sys.argv[1])
+filename = sys.argv[1]
+a = loadmat(filename)
 params = a['params'][0]
+print "## YAML script used to generate simulation in %s ##" % filename
 print '\n'.join(str(params).strip().strip('{ }').split(','))
 
